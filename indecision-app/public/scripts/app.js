5c8efd03c0e0f7027ddcf9c7ea37e4e5af543a8d
LuigiDerson <luigi.prodesign@gmail.com>
2019-09-07 04:33:06 +0200

    JSX expressions

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,10 +1,88 @@
-console.log('App.js is runinng')
+'use strict';
+
+console.log('App.js is runinng');
 
 // JSX - Javascript XML
-// var template = <p>This is JSX from app.js!</p>;
-var template = React.createElement("h1", {
-    id: "app"
-  }, "This is JSX from app.js!");
+var app = {
+  title: 'Indecision App',
+  subtitle: 'This is some info',
+  options: ['One', 'Two']
+};
+
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
+);
+
+// Learning section
+var user = {
+  name: 'Luigi',
+  age: 25,
+  location: 'Valencia'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age:',
+    user.age
+  ),
+  getLocation(user.location)
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
+
+/* babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+
+cd Desktop/react-course/indecision-app
+
+live-server public --browser=FIREFOX */
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,53 @@
 console.log('App.js is runinng')
 
 // JSX - Javascript XML
-var template = <p>This is JSX from app.js!</p>;
+const app = {
+  title: 'Indecision App',
+  subtitle: 'This is some info',
+  options: ['One', 'Two']
+}
+
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+
+
+// Learning section
+const user = {
+  name: 'Luigi',
+  age: 25,
+  location: 'Valencia'
+}
+
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>
+  }
+}
+
+const templateTwo = (
+  <div>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    {(user.age && user.age >= 18) && <p>Age:{user.age}</p>}
+    {getLocation(user.location)}
+  </div>
+);
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
+
+
+/* babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+
+cd Desktop/react-course/indecision-app
+
+live-server public --browser=FIREFOX */
