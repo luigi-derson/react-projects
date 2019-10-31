0d4ac3099c71cc6e969884d0ad405c001dc09973
LuigiDerson <luigi.prodesign@gmail.com>
2019-10-31 22:31:16 +0000

    Setup formating for amount and createdAt

--- expensify-app/src/components/ExpenseListItem.jsx
+++ expensify-app/src/components/ExpenseListItem.jsx
@@ -1,13 +1,19 @@
-/* eslint-disable */
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
+require("numeral/locales/en-gb")
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {numeral(amount / 100).format("$0,0.00")}
+            -
+            {moment(createdAt).format("MMMM Do, YYYY")}
+        </p>
     </div>
 );
 
